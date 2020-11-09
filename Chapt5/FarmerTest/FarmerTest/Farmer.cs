893c48d124f9db2602bb49edcc7367d23291f565
Giovanni Facchinetti <giovanni.facchinetti@ucdconnect.ie>
2020-11-09 15:39:51 +0000

    Implemented cow calculator - pp. 222-227

---
+++ Chapt5/FarmerTest/FarmerTest/Farmer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmerTest
+{
+    class Farmer
+    {
+        public int BagsOfFeed { get; private set; }
+        public int FeedMultiplier { get; }
+
+        private int numberOfCows;
+        public int NumberOfCows
+        {
+            get
+            {
+                return numberOfCows;
+            }
+            set
+            {
+                numberOfCows = value;
+                BagsOfFeed = FeedMultiplier * numberOfCows;
+            }
+        }
+
+        public Farmer(int numberOfCows, int feedMultiplier)
+        {
+            FeedMultiplier = feedMultiplier;
+            NumberOfCows = numberOfCows;
+        }
+    }
+}
