f3861682392de069c0e745961a74f62d19464324
Giovanni Facchinetti <giovanni.facchinetti@ucdconnect.ie>
2020-11-10 15:57:27 +0000

    Solved PoolPuzzle p. 263

---
+++ Chapt5/Chapt6PoolPuzzle/Chapt6PoolPuzzle/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapt6PoolPuzzle
+{
+    class Boat
+    {
+        private int length;
+
+        public void SetLength(int len)
+        {
+            length = len;
+        }
+
+        public int GetLength()
+        {
+            return length;
+        }
+
+        public virtual string Move()
+        {
+            return " drift ";
+        }
+    }
+
+    class Rowboat : Boat
+    {
+        public string RowTheBoat()
+        {
+            return "stroke natasha";
+        }
+    }
+
+    class Sailboat : Boat
+    {
+        public override string Move()
+        {
+            return " hoist sail ";
+        }
+    }
+
+    class TestBoats
+    {
+        static void Main()
+        {
+            string xyz = "";
+            Boat b1 = new Boat();
+            Sailboat b2 = new Sailboat();
+            Rowboat b3 = new Rowboat();
+
+            b2.SetLength(32);
+            xyz = b1.Move();
+            xyz += b3.Move();
+            xyz += b2.Move();
+
+            MessageBox.Show(xyz);
+        }
+    }
+}
