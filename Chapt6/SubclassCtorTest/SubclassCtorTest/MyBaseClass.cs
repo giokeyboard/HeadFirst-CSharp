2601b61a2d0d19541c01f438590675e0d7751366
Giovanni Facchinetti <giovanni.facchinetti@ucdconnect.ie>
2020-11-11 11:45:37 +0000

    Subclass constructor test p. 273

---
+++ Chapt6/SubclassCtorTest/SubclassCtorTest/MyBaseClass.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+
+namespace SubclassCtorTest
+{
+    class MyBaseClass
+    {
+        public MyBaseClass(string baseClassNeedsThis)
+        {
+            MessageBox.Show("This is the base class: " + baseClassNeedsThis);
+        }
+    }
+
+    class MySubClass : MyBaseClass
+    {
+        public MySubClass(string baseClassNeedsThis, int anotherValue) : base(baseClassNeedsThis)
+        {
+            MessageBox.Show("This is the subclass: " + baseClassNeedsThis + " and " + anotherValue);
+        }
+    }
+}
