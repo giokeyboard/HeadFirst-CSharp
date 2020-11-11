559f9f21d6a807c7e8b3e6172c622758c28c0204
Giovanni Facchinetti <giovanni.facchinetti@ucdconnect.ie>
2020-11-11 12:23:31 +0000

    Updated form files

--- Chapt6/DinnerPartyPlanner-v3.0/DinnerPartyPlanner-v3.0/Form1.cs
+++ Chapt6/DinnerPartyPlanner-v3.0/DinnerPartyPlanner-v3.0/Form1.cs
@@ -12,9 +12,67 @@ namespace DinnerPartyPlanner_v3._0
 {
     public partial class Form1 : Form
     {
+        DinnerParty dinnerParty;
+        BirthdayParty birthdayParty;
+
         public Form1()
         {
             InitializeComponent();
+            dinnerParty = new DinnerParty(5, healthyBox.Checked, fancyBox.Checked);
+            birthdayParty = new BirthdayParty(5, fancyBirthday.Checked, cakeWriting.Text);
+            DisplayDinnerPartyCost();
+            DisplayBirthdayPartyCost();
+        }
+
+        private void DisplayDinnerPartyCost()
+        {
+            decimal cost = dinnerParty.Cost;
+            costLabel.Text = cost.ToString("c");
+        }
+
+        private void DisplayBirthdayPartyCost()
+        {
+            tooLongLabel.Visible = birthdayParty.CakeWritingTooLong;
+            decimal cost = birthdayParty.Cost;
+            birthdayCost.Text = cost.ToString("c");
+        }
+
+        // Dinner Party controls' event handlers
+        private void numPeople_ValueChanged(object sender, EventArgs e)
+        {
+            dinnerParty.NumberOfPeople = (int)numPeople.Value;
+            DisplayDinnerPartyCost();
+        }
+
+        private void fancyBox_CheckedChanged(object sender, EventArgs e)
+        {
+            dinnerParty.FancyDecorations = fancyBox.Checked;
+            DisplayDinnerPartyCost();
+        }
+
+        private void healthyBox_CheckedChanged(object sender, EventArgs e)
+        {
+            dinnerParty.HealthyOption = healthyBox.Checked;
+            DisplayDinnerPartyCost();
+        }
+
+        // Birthday Party controls' event handlers
+        private void numberBirthday_ValueChanged(object sender, EventArgs e)
+        {
+            birthdayParty.NumberOfPeople = (int)numberBirthday.Value;
+            DisplayBirthdayPartyCost();
+        }
+
+        private void fancyBirthday_CheckedChanged(object sender, EventArgs e)
+        {
+            birthdayParty.FancyDecorations = fancyBirthday.Checked;
+            DisplayBirthdayPartyCost();
+        }
+
+        private void cakeWriting_TextChanged(object sender, EventArgs e)
+        {
+            birthdayParty.CakeWriting = cakeWriting.Text;
+            DisplayBirthdayPartyCost();
         }
     }
 }
