3be16a0d6687b766b8457d217e7738de94267142
Giovanni Facchinetti <giovanni.facchinetti@ucdconnect.ie>
2020-11-12 15:30:31 +0000

    Implement beehive mgmt sys pp. 279-285

--- Chapt6/BeehiveManagementSystem/BeehiveManagementSystem/Queen.cs
+++ Chapt6/BeehiveManagementSystem/BeehiveManagementSystem/Queen.cs
@@ -8,5 +8,64 @@ namespace BeehiveManagementSystem
 {
     class Queen
     {
+        /// <summary>
+        /// Track each of the worker bees and whether or not they have been assigned jobs
+        /// </summary>
+        private Worker[] workers;
+        private int shiftNumber;
+
+        public Queen(Worker[] workers)
+        {
+            this.workers = workers;
+        }
+
+        /// <summary>
+        /// Tell the workers to work and returns a shift report
+        /// </summary>
+        /// <param name="job"></param>
+        /// <param name="shifts"></param>
+        /// <returns></returns>
+        public bool AssignWork(string job, int shifts)
+        {
+            foreach (Worker worker in workers)
+            {
+                if (worker.DoThisJob(job, shifts))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Ask workers to work next shift and produce shift report
+        /// </summary>
+        /// <returns></returns>
+        public string WorkTheNextShift()
+        {
+            shiftNumber++;
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Report for shift #{shiftNumber}");
+
+            for (int i = 0; i < workers.Length; i++)
+            {
+                if (workers[i].DidYouFinish())
+                {
+                    sb.Append($"\r\nWorker #{i + 1} finished the job");
+                }
+                if (string.IsNullOrEmpty(workers[i].CurrentJob))
+                {
+                    sb.Append($"\r\nWorker #{i + 1} is not working");
+                }
+                else
+                {
+                    sb.Append($"\r\nWorker #{i + 1}");
+                    sb.Append((workers[i].ShiftsLeft > 0) ? $" is doing '{workers[i].CurrentJob}' for {workers[i].ShiftsLeft} more shifts" : $" will be done with '{workers[i].CurrentJob}' after this shift");
+                }
+
+            }
+
+            return sb.ToString();
+        }
     }
 }
--- Chapt6/BeehiveManagementSystem/BeehiveManagementSystem/Worker.cs
+++ Chapt6/BeehiveManagementSystem/BeehiveManagementSystem/Worker.cs
@@ -8,5 +8,52 @@ namespace BeehiveManagementSystem
 {
     class Worker
     {
+        private string[] jobsICanDo;
+        private int shiftsToWork;
+        private int shiftsWorked;
+
+        /// <summary>
+        /// What job the worker’s doing
+        /// </summary>
+        public string CurrentJob { get; private set; } = "";
+        public int ShiftsLeft { get { return shiftsToWork - shiftsWorked; } }
+
+        public Worker(string[] jobs)
+        {
+            jobsICanDo = jobs;
+        }
+
+        public bool DoThisJob(string job, int shifts)
+        {
+            if (!string.IsNullOrEmpty(CurrentJob)) { return false; }
+            foreach (string possibleJob in jobsICanDo)
+            {
+                if (possibleJob.Equals(job, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    CurrentJob = job;
+                    shiftsToWork = shifts;
+                    shiftsWorked = 0;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool DidYouFinish()
+        {
+            if (string.IsNullOrEmpty(CurrentJob)) { return false; }
+            shiftsWorked++;
+            if (shiftsWorked > shiftsToWork)
+            {
+                shiftsWorked = 0;
+                shiftsToWork = 0;
+                CurrentJob = "";
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
