5ffdc31480abaa049cbcdab38a76be705dca7975
Giovanni Facchinetti <giovanni.facchinetti@ucdconnect.ie>
2020-11-14 00:08:42 +0000

    Implemented first interfaces exercise p. 299

---
+++ Chapt7/InterfacesFirstApproach/InterfacesFirstApproach/Bee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace InterfacesFirstApproach
+{
+    class Bee : IStingPatrol
+    {
+        public int AlertLevel { get { return 0; } }
+
+        public int StingerLength { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public bool LookForEnemies()
+        {
+            return false;
+        }
+
+        public int SharpenStinger(int length)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Chapt7/InterfacesFirstApproach/InterfacesFirstApproach/IStingPatrol.cs
@@ -0,0 +1,11 @@
+﻿namespace InterfacesFirstApproach
+{
+    interface IStingPatrol
+    {
+        int AlertLevel { get; }
+        int StingerLength { get; set; }
+
+        bool LookForEnemies();
+        int SharpenStinger(int length);
+    }
+}
