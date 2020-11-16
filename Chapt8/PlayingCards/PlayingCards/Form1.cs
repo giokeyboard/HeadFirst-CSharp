f50ac2095f16aa1e1d109423a1fd414109c7e5c9
Giovanni Facchinetti <giovanni.facchinetti@ucdconnect.ie>
2020-11-16 15:37:13 +0000

    Implemented deck of cards using enums

---
+++ Chapt8/PlayingCards/PlayingCards/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlayingCards
+{
+    public partial class Form1 : Form
+    {
+        Random random;
+
+        public Form1()
+        {
+            InitializeComponent();
+            random = new Random();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Card card = new Card((Suits)random.Next(0, 4), (Values)random.Next(1, 14));
+            textBox1.Text = card.Name;
+        }
+    }
+}
