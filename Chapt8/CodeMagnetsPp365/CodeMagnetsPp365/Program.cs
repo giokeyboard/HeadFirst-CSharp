ab82a47684f4ba69c7f4abd056024cd4d90809fa
Giovanni Facchinetti <giovanni.facchinetti@ucdconnect.ie>
2020-11-16 16:49:17 +0000

    Implemented Code Magnets p. 365

---
+++ Chapt8/CodeMagnetsPp365/CodeMagnetsPp365/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodeMagnetsPp365
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> a = new List<string>();
+
+            string zilch = "zero";
+            string first = "one";
+            string second = "two";
+            string third = "three";
+            string fourth = "4.2";
+            string twopointtwo = "2.2";
+
+            a.Add(zilch);
+            a.Add(first);
+            a.Add(second);
+            a.Add(third);
+
+            a.RemoveAt(2);
+
+            if (a.Contains("two"))
+            {
+                a.Add(twopointtwo);
+            }
+
+            if (a.Contains("three"))
+            {
+                a.Add("four");
+            }
+
+            if (a.IndexOf("four") != 4)
+            {
+                a.Add(fourth);
+            }
+
+            printL(a);
+
+        }
+
+        public static void printL(List<string> a)
+        {
+            string result = "";
+
+            foreach (string element in a)
+            {
+                result += "\n" + element;
+            }
+
+            MessageBox.Show(result);
+        }
+    }
+}
