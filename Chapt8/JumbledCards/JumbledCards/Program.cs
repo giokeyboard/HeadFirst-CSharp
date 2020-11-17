abc11c2a8ba34919ea8e18660eb39846b07bc4e6
Giovanni Facchinetti <giovanni.facchinetti@ucdconnect.ie>
2020-11-17 18:52:21 +0000

    Implemented jumbled cards exercise p. 375

---
+++ Chapt8/JumbledCards/JumbledCards/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumbledCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            Console.WriteLine("Here's a list of random cards:");
+            List<Card> cards = new List<Card>();
+            for (int i = 0; i < 5; i++)
+            {
+                cards.Add(new Card((Suits)random.Next(0, 4), (Values)random.Next(1, 14)));
+                Console.WriteLine(cards[i].Name);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Those same cards, sorted:");
+            cards.Sort(new CardComparer_byValue());
+            foreach (Card card in cards)
+            {
+                Console.WriteLine(card.Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class CardComparer_byValue : IComparer<Card>
+    {
+        public int Compare(Card x, Card y)
+        {
+            if (x.Value > y.Value) { return 1; }
+            else if (x.Value < y.Value) { return -1; }
+            else
+            {
+                if (x.Suit > y.Suit) { return 1; }
+                else if (x.Suit < y.Suit) { return -1; }
+                else { return 0; }
+            }
+        }
+    }
+}
