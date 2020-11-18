710c9de98478bba487b3582c78e031640988dcb2
Giovanni Facchinetti <giovanni.facchinetti@ucdconnect.ie>
2020-11-18 17:17:08 +0000

    Implemented overloading example using cards

---
+++ Chapt8/OverloadingExample/OverloadingExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card cardToCheck = new Card(Suits.Clubs, Values.Three);
+            Console.WriteLine(Card.DoesCardMatch(cardToCheck, Suits.Hearts));
+            Console.WriteLine(Card.DoesCardMatch(cardToCheck,Values.Three));
+        }
+    }
+}
