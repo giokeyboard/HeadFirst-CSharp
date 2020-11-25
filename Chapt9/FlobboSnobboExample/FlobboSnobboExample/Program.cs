11b52f5840f8eb4e4413dbacc466c3278e0d75b7
Giovanni Facchinetti <giovanni.facchinetti@ucdconnect.ie>
2020-11-25 15:55:37 +0000

    Implemented StreamWriter magnets exercise p. 415

---
+++ Chapt9/FlobboSnobboExample/FlobboSnobboExample/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlobboSnobboExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Flobbo f = new Flobbo("blue yellow");
+            StreamWriter sw = f.Snobbo();
+            f.Blobbo(f.Blobbo(f.Blobbo(sw), sw), sw);
+        }
+    }
+
+    class Flobbo
+    {
+        private string zap;
+        public Flobbo(string zap)
+        {
+            this.zap = zap;
+        }
+
+        public bool Blobbo(bool Already, StreamWriter sw)
+        {
+            if (Already)
+            {
+                sw.WriteLine(zap);
+                sw.Close();
+                return false;
+
+            }
+            else
+            {
+                sw.WriteLine(zap);
+                zap = "red orange";
+                return true;
+            }
+        }
+
+        public bool Blobbo(StreamWriter sw)
+        {
+            sw.WriteLine(zap);
+            zap = "green purple";
+            return false;
+        }
+
+        public StreamWriter Snobbo()
+        {
+            return new StreamWriter(@"C:\Users\gfacchin\Documents\Courses\macaw.txt");
+        }
+    }
+}
