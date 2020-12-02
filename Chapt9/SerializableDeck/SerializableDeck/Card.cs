63abbd36259ed3521e78ce86005e9584227478b3
Giovanni Facchinetti <giovanni.facchinetti@ucdconnect.ie>
2020-12-02 22:45:45 +0000

    Implemented DeckOfCards serializable to binary files

---
+++ Chapt9/SerializableDeck/SerializableDeck/Card.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializableDeck
+{
+    [Serializable]
+    class Card
+    {
+        public Suits Suit { get; private set; }
+        public Values Value { get; private set; }
+        public string Name { get { return $"{Value} of {Suit}"; } }
+
+        public Card(Suits suit, Values value)
+        {
+            Suit = suit;
+            Value = value;
+        }
+
+        public static string Plural(Values value)
+        {
+            if (value == Values.Six)
+                return "Sixes";
+            else
+                return value.ToString() + "s";
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public enum Values
+    {
+        Ace = 1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King,
+    }
+
+    public enum Suits
+    {
+        Spades = 0,
+        Clubs = 1,
+        Diamonds = 2,
+        Hearts = 3,
+    }
+}
---
+++ Chapt9/SerializableDeck/SerializableDeck/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializableDeck
+{
+    class Program
+    {
+        Random random = new Random();
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a random deck of cards
+        /// </summary>
+        private Deck RandomDeck(int number)
+        {
+            Deck myDeck = new Deck(new Card[] { });
+            for (int i = 0; i < number; i++)
+            {
+                myDeck.Add(new Card(
+                (Suits)random.Next(4),
+                (Values)random.Next(1, 14)
+                ));
+            }
+            return myDeck;
+        }
+
+        /// <summary>
+        /// Deals all of the cards and prints them to the console
+        /// </summary>
+        private void DealCards(Deck deckToDeal, string title)
+        {
+            Console.WriteLine(title);
+            while (deckToDeal.Count > 0)
+            {
+                Card nextCard = deckToDeal.Deal(0);
+                Console.WriteLine(nextCard.Name);
+            }
+            Console.WriteLine("------------------");
+        }
+    }
+}
